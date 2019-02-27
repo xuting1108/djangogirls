8c29a2627e970c1906f1f47f7309c530eb478d49
Filipe <xuting1108@hotmail.com>
2019-02-27 09:27:04 -0300

    segundo commit

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+#Aqui, estamos importando do Django
+# a função url e todas as nossas views do aplicativo blog
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+#Como você pode ver, estamos agora atribuindo uma view
+#chamada post_list à URL raiz.
+#A última parte, name='post_list', é o nome da URL que será usado para identificar a view.
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+#o "render" vai renderizar o modelo de acordo com o template
+# blog/post_list e retornara o resultado
