40528f93ca70d2144f4a8ca8610e75effdc5da79
Filipe <xuting1108@hotmail.com>
2019-02-27 10:37:05 -0300

    terceiro commit

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now())\
+        .order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 #o "render" vai renderizar o modelo de acordo com o template
 # blog/post_list e retornara o resultado
